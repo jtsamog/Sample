8f1753880b16a8f9883f905d3bee12671699bad9
Sam Ogunyowa <mailsamog@gmail.com>
2017-06-26 00:29:56 -0400

    Initial Commit

---
+++ Sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample
+//
+//  Created by Endeavour2 on 6/26/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        //jjj
+    }
+    return 0;
+}
